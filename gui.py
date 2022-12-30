027af1eb19c26473a08114a51a50fc59787dfcc4
SurveSan <120928723+Surveinit@users.noreply.github.com>
2022-12-30 14:39:25 +0530

    Nothing new

--- gui.py
+++ gui.py
@@ -1,3 +1,4 @@
+#branch color change
 from cgitb import text
 from tkinter import *
 from turtle import width
@@ -5,25 +6,25 @@ from pass_generator import *
 
 window = Tk()
 window.title("PassGen")
-window.config(bg="#121212")
+window.config(bg="#202124")
  
 def gen():
 
     if(scale.get()==8):
         output = Label(text=pass_gen_e(),
-                font=("impact",20), bg="#121212", fg="#ebebeb")
+                font=("impact",20), bg="#202124", fg="#d80025")
     
         output.pack()
 
     elif(scale.get()==10):
         output = Label(text=pass_gen(),
-                font=("impact",20), bg="#121212", fg="#ebebeb")
+                font=("impact",20), bg="#202124", fg="#ebc531")
     
         output.pack()
 
     else:
         output = Label(text=pass_gen_t(),
-                font=("impact",20), bg="#121212", fg="#ebebeb")
+                font=("impact",20), bg="#202124", fg="#4ba641")
     
         output.pack()
 
@@ -31,7 +32,7 @@ def gen():
 
 #heading
 heading = Label(text="Pass-Generator",
-                font=("impact",30), fg="#eb4034",bg="#121212")
+                font=("impact",30), fg="#eb4034",bg="#202124")
 
 heading.pack()
  
